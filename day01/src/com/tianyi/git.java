4dcd60a741c30b5f6a15183582406d44f7dbdcc8
chen ren <1030506495@qq.com>
2019-10-09 18:20:20 +0800

    2019 10 9

---
+++ day01/src/com/tianyi/git.java
@@ -0,0 +1,11 @@
+package com.tianyi;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("学习git的第一天");
+        System.out.println("学习git的第一天");
+        System.out.println("学习git的第一天");
+        System.out.println("学习git的第一天");
+
+    }
+}
